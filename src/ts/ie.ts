16bd894eaefcb32f77b288a27fe4a0686aea624c
Taiji Kondo <>
2021-01-27 20:52:13 +0900

    🔧 Add IE polyfill

--- src/ts/ie.ts
+++ src/ts/ie.ts
@@ -1,4 +1,9 @@
 import 'picturefill';
 import 'picturefill/dist/plugins/intrinsic-dimension/pf.intrinsic.min.js';
+import 'nodelist-foreach-polyfill';
+import objectFitImages from 'object-fit-images';
 
-require('nodelist-foreach-polyfill');
+window.addEventListener('DOMContentLoaded', () => {
+  const images: NodeListOf<HTMLImageElement> = document.querySelectorAll('img');
+  objectFitImages(images);
+});
